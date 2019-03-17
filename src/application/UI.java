5d2fe7519158ed4409158a64e7cad70d5d369c84
Leonel Gallo <leonel.gallo@gmail.com>
2019-03-17 00:29:27 -0300

    UI - Format

--- src/application/UI.java
+++ src/application/UI.java
@@ -119,8 +119,6 @@ public class UI {
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.print(ANSI_RESET);
-		
-		
+		System.out.print(ANSI_RESET);		
 	}
 }
