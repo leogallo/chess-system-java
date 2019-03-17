8b70a334eefae6cb9b48067afb056d4b36284445
Leonel Gallo <leonel.gallo@gmail.com>
2019-03-17 00:37:19 -0300

    UI - Format

--- src/application/UI.java
+++ src/application/UI.java
@@ -111,6 +111,7 @@ public class UI {
 	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		
 		System.out.println("Captured pieces:");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
